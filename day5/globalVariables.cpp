387eeeddf826b0a443000c0dd7023c370c78af1a
kkotsche1 <kkotsche1@gmail.com>
2023-12-17 15:09:57 +0100

    more global variables

--- day5/globalVariables.cpp
+++ day5/globalVariables.cpp
@@ -2,9 +2,11 @@
 #include "includes/myfile.h"
 
 int x = 1000;
+extern int test;
 
 int main()
 {
     std::cout << add_to_x(100) << std::endl;
+    std::cout << test << std::endl;
     return 0;
 }
--- day5/includes/myfile.cpp
+++ day5/includes/myfile.cpp
@@ -1,5 +1,7 @@
 extern int x;
 
+int test = 999;
+
 int add_to_x(int i)
 {
     return x + i;
