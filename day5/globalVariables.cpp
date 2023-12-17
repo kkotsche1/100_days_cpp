70db5e6fe396d7dcc69a8dcbf79f849e6af2519e
kkotsche1 <kkotsche1@gmail.com>
2023-12-17 18:35:43 +0100

    update

--- day5/globalVariables.cpp
+++ day5/globalVariables.cpp
@@ -4,6 +4,12 @@
 int x = 1000;
 extern int test;
 
+// Defining a global variable, but only within this file necesitates a static tag
+// The variable can be used with any function or code in this particular file
+// but is not accessible via the extern keyword from other files
+
+static int semiGlobal = 785;
+
 int main()
 {
     std::cout << add_to_x(100) << std::endl;
