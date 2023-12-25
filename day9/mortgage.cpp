fb6a5efb48f4238b91b97bb34b7fe8adf2f91205
kkotsche1 <kkotsche1@gmail.com>
2023-12-25 17:20:51 +0100

    Loan Repayment Calculator finished

--- day9/mortgage.cpp
+++ day9/mortgage.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 #define MONTHS_IN_YEAR 12
 
@@ -7,7 +8,7 @@ int main()
 {
 
     // Asking the user to enter the principal amount of their loan
-    std::cout << "Please enter a numeric value for the principal amount of your loand" << std::endl;
+    std::cout << "Please enter a numeric value for the principal amount of your loan" << std::endl;
     double principal = 0.0; // When we declare our new variable we assign it some value to reduce error likelihood
     std::cin >> principal;
 
@@ -24,11 +25,38 @@ int main()
     double monthlyInterest = interest / MONTHS_IN_YEAR;
     long monthsOfLoan = loanYears * MONTHS_IN_YEAR;
 
+    // Initializing a variable to calculate our monthly payment
     double monthlyPayment = 0.0;
 
+    // Calculating our monhtly payment
     monthlyPayment = principal * (monthlyInterest / (1 - (pow(1 + monthlyInterest, -monthsOfLoan))));
 
-    std::cout << "The monthly mortgage payment is: " << monthlyPayment << std::endl;
+    // Informing the user of their monthly payment
+    std::cout << "The monthly mortgage payment is: " << std::fixed << std::setprecision(2) << monthlyPayment << std::endl;
+
+    // Initializing variables to track the payments we make over time
+    double currInterestPayment = 0.0;
+    double currPrincipalPayment = 0.0;
+    double currBalance = principal;
+    double totalInterestPaid = 0.0;
+
+    // Initializing a variable which we will use to loop through all of the payment months
+    long currentLoanMonth = 1;
+
+    // Looping through our payment months
+    while (currentLoanMonth <= monthsOfLoan)
+    {
+
+        currInterestPayment = currBalance * monthlyInterest;
+        totalInterestPaid = totalInterestPaid + currInterestPayment;
+        currPrincipalPayment = monthlyPayment - currInterestPayment;
+        currBalance = currBalance - currPrincipalPayment;
+        std::cout << "Month Number: " << currentLoanMonth << "      "
+                  << "Interest Payment: " << currInterestPayment << "   Principal Payment: " << currPrincipalPayment << "   Remaining Balance: " << currBalance << std::endl;
+        currentLoanMonth++;
+    }
+
+    std::cout << "Total amount of interes paid: " << totalInterestPaid << std::endl;
 
     return 0;
 }
