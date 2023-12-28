5da899317221a0caf91baee2af6c9e356f4bb172
kkotsche1 <kkotsche1@gmail.com>
2023-12-28 20:08:32 +0100

    update

---
+++ day10/ValRefPointIndPoint.cpp
@@ -0,0 +1,7 @@
+// Passing by value, references, pointers and indirect pointers
+
+int main()
+{
+
+    return 0;
+}
--- day10/pointers2.cpp
+++ day10/pointers2.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <iomanip>
 
+int &maxNum(int &firstN, int &secondN)
+{
+    if (firstN > secondN)
+    {
+        return firstN;
+    }
+    else
+    {
+        return secondN;
+    }
+}
+
 int main(int argc, const char *argv[])
 {
-
     int anInt = 0;
     float aFloat = 0.0;
     double aDouble = 0.0;
@@ -33,13 +44,26 @@ int main(int argc, const char *argv[])
     // We can also create conoined variables using pointer notation
     int aNewInt = 10;
 
+    // aNewIntReference now holds the actual value that is being stored for the "aNewInt" variable
     int &aNewIntReference = aNewInt;
 
-    std::cout << aNewIntReference << std::endl;
+    std::cout << aNewIntReference << std::endl; // 10
 
+    // Incrementing the "aNewInt" variable will also increment the "aNewIntReference" varibale
     aNewInt++;
 
-    std::cout << aNewIntReference << std::endl;
+    std::cout << aNewIntReference << std::endl; // 11
+
+    // New Section - Use of References
+    int firstNum = 42;
+    int secondNum = 69;
+
+    // We can also pass references to variables to certain functions. In this case, maxNum returns the reference to
+    // firstNum or secondNum depending on which is larger. Since the function returns the reference to that variable
+    // we can go ahead and set it to 99 which makes the value of the variable 99
+    maxNum(firstNum, secondNum) = 99;
+
+    std::cout << "First Num: " << firstNum << "     Second Num: " << secondNum << std::endl;
 
     return 0;
 }
