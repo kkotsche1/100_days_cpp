fbfda416ffd4507c62ed40b657f453b4a16e1ed0
kkotsche1 <kkotsche1@gmail.com>
2024-01-03 22:40:21 +0100

    class updates

--- day13/Bond.cpp
+++ day13/Bond.cpp
@@ -16,3 +16,38 @@ Bond::Bond()
     yearsToMaturity = 1;
     paymentType = 'A';
 }
+
+// Defining the functions we have placed in our Bond.h file
+
+void Bond::setPrincipal(double prin)
+{
+    principal = prin;
+};
+double Bond::getPrincipal()
+{
+    return principal;
+};
+void Bond::setRate(double rat)
+{
+    rate = rat;
+};
+double Bond::getRate()
+{
+    return rate;
+};
+void Bond::setYearsToMaturity(int yTM)
+{
+    yearsToMaturity = yTM;
+};
+int Bond::getYearsToMaturity()
+{
+    return yearsToMaturity;
+};
+void Bond::setPaymentType(char pType)
+{
+    paymentType = pType;
+};
+char Bond::getPaymentType()
+{
+    return paymentType;
+};
--- day13/ClassesMain.cpp
+++ day13/ClassesMain.cpp
@@ -11,7 +11,16 @@ int main(int argc, const char *argv[])
 
     // We can access the public variable and functions associated with this class. In this
     // case the variables have been set via the constructor function
-    std::cout << "Bond Principal: " << basicBond.principal << std::endl; // prints Bond Principal: 100
+    // std::cout << "Bond Principal: " << basicBond.principal << std::endl; // prints Bond Principal: 100
+
+    // If the variable are private we can access them via getter and setter functions as follows
+
+    int yearsToMaturity = basicBond.getYearsToMaturity();
+    std::cout << "Years to Maturity: " << yearsToMaturity << std::endl;
+    std::cout << "Setting Bond Maturity to 5 Years" << std::endl;
+
+    basicBond.setYearsToMaturity(5);
+    std::cout << "Years to Maturity New: " << basicBond.getYearsToMaturity() << std::endl;
 
     return 0;
 }
